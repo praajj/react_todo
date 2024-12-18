ef2c97517f2d8de4225c3b4ce39bdfb14090bc08
praajj <parteprajaktaa@gmail.com>
2024-12-18 14:44:07 +0530

    displaying the task

--- src/components/Todo.js
+++ src/components/Todo.js
@@ -1,7 +1,24 @@
-import React from 'react'
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faPenToSquare } from "@fortawesome/free-solid-svg-icons";
+import { faTrash } from "@fortawesome/free-solid-svg-icons";
 
-export const Todo = () => {
+export const Todo = ({task}) => {
   return (
-    <div>Todo</div>
-  )
-}
+    <div className="Todo">
+      <p>{task.task}</p>
+      <div>
+        <FontAwesomeIcon
+          className="edit-icon"
+          icon={faPenToSquare}
+          //onClick={() => editTodo(task.id)}
+        />
+        <FontAwesomeIcon
+          className="delete-icon"
+          icon={faTrash}
+          //onClick={() => deleteTodo(task.id)}
+        />
+      </div>
+    </div>
+  );
+};
--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -1,6 +1,8 @@
 import React, { useState } from "react";
+import { Todo } from "./Todo";
 import { TodoForm } from "./TodoForm";
 import { v4 as uuidv4 } from "uuid";
+import { EditTodoForm } from "./EditTodoForm";
 uuidv4();
 
 export const TodoWrapper = () => {
@@ -9,13 +11,16 @@ export const TodoWrapper = () => {
     setTodos([
       ...todos, // spread operator
       { id: uuidv4(), task: todo, completed: false, isEditing: false },
-    ])
+    ]);
     console.log("todos =", todos);
   };
   return (
     <div className="TodoWrapper">
       <h1>Get Things Done !</h1>
       <TodoForm addTodo={addTodo} />
+      {todos.map((todo, index) => (
+        <Todo task={todo} key={index} />
+      ))}
     </div>
   );
 };
