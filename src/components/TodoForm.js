63071b071d70d078065f59b3e6c851e29f7fd0fd
praajj <parteprajaktaa@gmail.com>
2024-12-18 13:43:07 +0530

    add the 'task for the day' btn

--- src/components/TodoForm.js
+++ src/components/TodoForm.js
@@ -2,6 +2,13 @@ import React from 'react'
 
 export const TodoForm = () => {
   return (
-    <div>TodoForm</div>
-  )
+    <form className="TodoForm">
+      <input
+        type="text"
+        className="todo-input"
+        placeholder="Task for the day"
+      />
+      <button type="submit" className="todo-btn"></button>
+    </form>
+  );
 }
--- src/components/TodoWrapper.js
+++ src/components/TodoWrapper.js
@@ -1,7 +1,9 @@
 import React from 'react'
+import { TodoForm } from './TodoForm'
 
 export const TodoWrapper = () => {
   return (
-    <div>TodoWrapper</div>
+    <div className='TodoWrapper'>
+      <TodoForm/></div>
   )
 }
